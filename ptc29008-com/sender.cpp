6622b6886bd6c4ddc4ce408bb8b7c3a2e7ab7c48
werner <wernerthiago@gmail.com>
2016-03-15 16:12:53 -0300

    Sender tun.

--- ptc29008-com/sender.cpp
+++ ptc29008-com/sender.cpp
@@ -4,22 +4,20 @@
 int main(){
 	APC220 sender;
 	tun tun;
+	char data;
+	char * buffer;
+	int i = 0;
 
-	char * payload = "werner1";
-	sender.setPTC(PT1);
-	cout << "SEQ" << sender.getSEQ() << ": " << payload << endl;
-	bool aux = sender.send(payload);
-
-	payload = "werner2";
-	cout << "SEQ" << sender.getSEQ() << ": " << payload << endl;
-	aux = sender.send(payload);
-
-	tun.tun_alloc("tun0");
+	int fd = tun.tun_alloc("tun0");
 	if (tun.set_ip("tun0", "10.0.0.2", "10.0.0.1") < 0) {
 		perror("so configurar a interface tun");
 		return 0;
 	}
-	while(1);
+	while(1){
+		if(read(fd,buffer+3,MAX_LENGTH) > 0){
+			sender.send(buffer);
+		}
+	}
 
 	sender.closed();
 	return 0;
